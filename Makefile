74192f67af16a40fed3eae96f2bd19424854ce5e
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2025-09-19 00:08:05 -0400

    fix: updates
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

---
+++ Makefile
@@ -0,0 +1,42 @@
+DOCKER_COMPOSE:=docker compose --file docker-compose.yaml --file compose/dapr-docker-compose.yaml
+PUBSUB_NAME:=pubsub
+TOPIC_NAME:=counter
+PAYLOAD:=3
+
+define EVENT_PAYLOAD
+endef
+
+start:
+	$(DOCKER_COMPOSE) up -d
+
+stop:
+	$(DOCKER_COMPOSE) rm --stop --force
+
+pull:
+	$(DOCKER_COMPOSE) pull
+
+processor.logs:
+	$(DOCKER_COMPOSE) logs -f queueprocessor
+
+restart: stop start
+
+pub:
+	$(DOCKER_COMPOSE) exec queueprocessor \
+		curl "http://localhost:3500/v1.0/publish/$(PUBSUB_NAME)/$(TOPIC_NAME)?metadata.rawPayload=false" \
+		--header "Content-Type: application/json" \
+		--data 3
+
+counter:
+	@curl -s -X POST http://localhost:5000/counter -H "Content-Type: application/json" -d '2' | jq .
+
+get:
+	@curl -s -X GET http://localhost:5000/ -H "Content-Type: application/json" | jq .
+
+pending:
+	redis-cli XRANGE $(TOPIC_NAME) - +
+
+clear:
+	redis-cli XTRIM $(TOPIC_NAME) MAXLEN = 0
+
+monitor:
+	redis-cli MONITOR
---
+++ src/Dapr.Demo.QueueProcessor/Program.cs
@@ -0,0 +1,34 @@
+using Dapr.Client;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestHeaders.Add("Pubsubname");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+});
+
+builder.Services.AddSingleton(new DaprClientBuilder().Build());
+
+var app = builder.Build();
+
+app.UseCloudEvents();
+
+app.MapSubscribeHandler();
+
+app.MapGet("/", async (DaprClient dapr) => await dapr.GetStateAsync<int>("statestore", "counter"));
+
+app.MapPost("/counter", async ([FromBody] int counter, ILogger<Program> logger, DaprClient dapr) =>
+{
+    var newCounter = counter * counter;
+    logger.LogInformation("Updating counter: {newCounter}", newCounter);
+    // Save state out to a data store.  We don't care which one!
+    await dapr.SaveStateAsync("statestore", "counter", newCounter);
+    return Results.Accepted("/", newCounter);
+}).WithTopic("pubsub", "counter", false);
+
+app.Run();
